e835210ff8c7b7d6c6e933291b1485afa0b5378b
sazuko07 <sazu07@gmail.com>
2024-09-18 17:57:03 -0500

    dealership 2nd push

--- src/com/skilldistillery/dealership/app/CarTester.java
+++ src/com/skilldistillery/dealership/app/CarTester.java
@@ -3,27 +3,30 @@ import com.skilldistillery.dealership.entity.*;
 public class CarTester {
 
   public static void main(String[] args) {
+	  
     // Create two Cars and assign fields
     Car fredsCar = new Car();
-    fredsCar.make = "Kia";
-    fredsCar.model = "Sportage";
-    fredsCar.color = "Hot Pink";
-    fredsCar.purchasePrice = 28565;
+    fredsCar.setMake("Kia");
+    fredsCar.setModel("Sportage");
+    fredsCar.setColor("Hot Pink");
+    fredsCar.setPurchasePrice(28565);
+    System.out.println();
 
     Car bernicesCar = new Car();
 
-    bernicesCar.make = "Toyota";
-    bernicesCar.model = "Prius";
-    bernicesCar.color = "Neon";
-    bernicesCar.purchasePrice = 32199;
+    bernicesCar.setMake("Toyota");
+    bernicesCar.setModel("Prius");
+    bernicesCar.setColor("Neon");
+    bernicesCar.setPurchasePrice(32199);
+    System.out.println();
 
     // Have the cars display themselves
     fredsCar.displayCar();
     bernicesCar.displayCar();
     System.out.println();
 
-    String fredCarData = fredsCar.getCarData();
-    String bernicesCarData = bernicesCar.getCarData();
+    String fredCarData = fredsCar.getCarInfo();
+    String bernicesCarData = bernicesCar.getCarInfo();
 
     // Get the cars' data and display that
     System.out.println("Fred car data: " + fredCarData);
--- src/com/skilldistillery/dealership/entity/Car.java
+++ src/com/skilldistillery/dealership/entity/Car.java
@@ -1,18 +1,69 @@
 package com.skilldistillery.dealership.entity;
 
 public class Car {
-   public String make;
-   public String model;
-   public String color;
-   public double purchasePrice;
+	private String make;
+	private String model;
+	private String color;
+	private double purchasePrice;
+	private String getCarInfo;
 
-   public String getCarData() {
-      String output = "make=" + make + ", model=" + model + ", color=" + color + ", purchasePrice=" + purchasePrice;
-      return output;
-   }
-
-   public void displayCar() {
-      String carData = getCarData();
+   
+	//display method for car
+	public void displayCar() {
+      String carData = getCarInfo;
       System.out.println(carData);
    }
-}
+	//car constructor with the 4 variables
+   public Car(String make, String model, String color, double purchasePrice) {
+	   this.make = make;
+	   this.model = model;
+	   this.color = color;
+	   this.purchasePrice = purchasePrice;
+   }
+   	
+   //toString replacement with similar format (reads like a list, instead of just left to right)
+   public String getCarInfo() {
+	   System.out.println("Here are the specs for the car: ");
+	   System.out.println("make: " +make);
+	   System.out.println("model: " +model);
+	   System.out.println("color: " +color);
+	   System.out.println("purchase price: " +purchasePrice);
+	  
+	   return getCarInfo;
+   }
+   
+   //an example of a car (to set default values)
+   public Car() {
+   		this.make = "Ford";
+   		this.model = "Explorer";
+   		this.color = "white";
+   		this.purchasePrice = 49995.63;
+   		
+   //getters and setters to pass private fields
+   }
+   public String getMake (String make) {
+	   return this.make;
+   }
+   public void setMake (String make) {
+	   this.make = make;
+   }
+   public String getModel (String model) {
+	   return this.model;
+   }
+   public void setModel (String model) {
+	   this.model = model;
+   }
+   public String getColor (String color) {
+	   return this.color;
+   }
+   public void setColor (String color) {
+	   this.color = color;  
+   }
+   public double getuPrchasePrice (double purchasePrice) {
+	   return this.purchasePrice;
+   }
+   public void setPurchasePrice (double purchasePrice) {
+	   this.purchasePrice = purchasePrice; 
+   }
+   
+  }
